6f06224820480a2333b22e936824c1f1429564b7
Jihoon Yoon <jihoonyoon0@gmail.com>
2022-12-22 11:51:42 +0000

    Update main to personalize bot usage

--- main.py
+++ main.py
@@ -5,18 +5,24 @@
 import discord
 import os
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents(members=True, messages=True, guilds=True)
+client = discord.Client(intents=intents)
+# Lets the bot listen for specific messages
+intents.message_content = True
+
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+  if message.content.startswith('$greetings'):
+    await message.channel.send(f'Hello {message.author}')
+
 
 client.run(os.getenv('TOKEN'))
